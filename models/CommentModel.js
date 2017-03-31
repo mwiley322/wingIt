e8f19c8f76ae900ba5e3210f63728c8b400ab526
Megan Wiley <mwiley322@gmail.com>
2017-03-31 15:17:05 -0700

    Comment model created

---
+++ models/CommentModel.js
@@ -0,0 +1,14 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var User = require('./UserModel');
+var Post = require('./PostModel');
+
+var CommentSchema = new Schema({
+  _author: [ User.schema.username ],
+  text: String,
+  _post: [ Post.schema ]
+});
+
+module.exports = mongoose.model('Comment', CommentSchema);
