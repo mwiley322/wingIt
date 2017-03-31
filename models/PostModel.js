b76d1cd463f8b1c95cb167fc979561a5ba635fa5
Lamar Smith <DreamBig@Laillahx.local>
2017-03-31 14:28:41 -0700

    Work on post model

---
+++ models/PostModel.js
@@ -0,0 +1,13 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var PostSchema = new Schema({
+  author: [ User.name ],
+  title: String,
+  city: [ City.name ],
+  text: String
+});
+
+// var Post = mongoose.model('Post', PostSchema);
+module.exports = mongoose.model('Post', PostSchema);
