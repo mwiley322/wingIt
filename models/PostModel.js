6799bb7c24f9e617f6344f495946529fd9857361
Lamar Smith <DreamBig@Laillahx.local>
2017-04-01 19:00:27 -0700

    forgot to correct post model

--- models/PostModel.js
+++ models/PostModel.js
@@ -6,19 +6,10 @@ var Schema = mongoose.Schema;
 var User = require('./UserModel');
 
 var PostSchema = new Schema({
-<<<<<<< HEAD
-  _author: [ UserSchema.username ],
-  title: String,
-  city: [ CitySchema.name ],
-  text: String
-});
-// var Post = mongoose.model('Post', PostSchema);
-=======
   author: String,
   title: String,
   city: String,
   content: String
 });
 
->>>>>>> 5b7f8da865cba3fb89b5ac1ea544ec40f8e0cd3e
 module.exports = mongoose.model('Post', PostSchema);
