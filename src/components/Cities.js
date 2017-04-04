31e3569039aaf2d1a765c22ccfd52efb4ba5e0e3
Lamar Smith <DreamBig@Laillahx.local>
2017-04-03 20:30:57 -0700

    formated city and post components

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import style from './index.css'
-
-
 import {oneCity} from './Util';
 import Post from './Post';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 
 class Cities extends Component {
@@ -41,12 +41,12 @@ class Cities extends Component {
                              <h4 className="cityPop">Population:{city.Population}</h4>
                               <img src={city.imageUrl} style={style.cityImage} alt={city.name} classNameName="img-responsive"/>
                          </div>
-                         <div className="ratings" key ={city._id}>
-                          
+                         <div className="ratings" key={city._id}>
+
                           </div>
                              <h3>Affordable: ({city.isAffordable} ? $ : $$$)</h3>
         <button onClick={this.getCityProfile.bind(this, city._id)}>See City Profile</button>
-          <Post posts = {this.state.posts}/>
+          <Post posts={this.state.posts}/>
                              <p>
                                  <span className="glyphicon glyphicon-star"></span>
                                  <span className="glyphicon glyphicon-star"></span>
@@ -55,8 +55,10 @@ class Cities extends Component {
                                  <span className="glyphicon glyphicon-star-empty"></span>
                                  4.0 stars
                              </p>
+
                          </div>
                      </div>
+
         )
      })
       return (
