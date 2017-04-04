c3c483a63a1c1f176cfb49866a1df195fcb63c96
Lamar Smith <DreamBig@Laillahx.local>
2017-04-03 18:52:38 -0700

    formated city page

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
-import style from './style'
+import style from './index.css'
+
 
 class Cities extends Component {
     render() {
       let cities = this.props.cities
       let results = cities.map( (city, index) => {
         return (
-          <div key ={index}>
-          <img src = {city.imageUrl} style ={style.cityImage} alt={city.name}/>
-          <h1>Name:{city.name}</h1>
-          <h3>Population:{city.Population}</h3>
-          <h3>Affordable: ({city.isAffordable} ? $ : $$$)</h3>
-          </div>
+          <div className="col-md-9">
+                     <div className="thumbnail">
+                         <div className="caption-full">
+                           <h2 className="cityName">{city.name}</h2>
+                             <h4 className="cityPop">Population:{city.Population}</h4>
+                              <img src={city.imageUrl} style={style.cityImage} alt={city.name} classNameName="img-responsive"/>
+                         </div>
+                         <div className="ratings">
+                          </div>
+                             <h3>Affordable: ({city.isAffordable} ? $ : $$$)</h3>
+                             <p>
+                                 <span className="glyphicon glyphicon-star"></span>
+                                 <span className="glyphicon glyphicon-star"></span>
+                                 <span className="glyphicon glyphicon-star"></span>
+                                 <span className="glyphicon glyphicon-star"></span>
+                                 <span className="glyphicon glyphicon-star-empty"></span>
+                                 4.0 stars
+                             </p>
+                         </div>
+                     </div>
         )
      })
       return (
