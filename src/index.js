e3416ba4d8626f7b439cdaef4fb05ec317643df2
kashifali554 <kashifali874@gmail.com>
2017-04-06 14:57:43 -0700

    Edit comment component

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Profile from './components/Profile';
 import { browserHistory } from 'react-router';
 import {HashRouter, Route} from 'react-router-dom';
 import { requireAuth } from './components/AuthService';
+import EditPost from './components/EditPost.js';
 
 
 
@@ -16,6 +17,9 @@ const Root = () => {
     </HashRouter>
     <HashRouter history={browserHistory}>
       <Route path="/profile" component={Profile} onEnter={requireAuth} />
+    </HashRouter>
+    <HashRouter history={browserHistory}>
+      <Route path="/edit" component={EditPost} onEnter={requireAuth} />
     </HashRouter>
     </div>
   )
